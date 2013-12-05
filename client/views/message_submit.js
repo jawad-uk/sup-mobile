a853b3fad2a886ce50b32276f49d1f5db403bfd3
Jawad Abdulsamad <jhsamad@gmail.com>
2013-12-05 22:06:13 +0300

    added username to conv

--- client/views/message_submit.js
+++ client/views/message_submit.js
@@ -2,10 +2,13 @@ Template.messageSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
 		console.log('new comment created')
+		
+		var user = Meteor.user(); 
 
 		var message = {
 			msgText: $(e.target).find('[name=msg]').val(),
-			sentBy: 'anonymous'
+			sentBy: user.profile.name,
+			userId: user._id
 		}
 		message._id = Messages.insert(message);
 		$('.pure-input-rounded').val("");
